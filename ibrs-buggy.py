0c22c80d882cf56c0db0219fdf27edbd0bc34f1c
Roy H <maro@nextco.me>
2017-11-16 23:48:57 -0500

    Added ibrs.py -> Good Version for analisis

---
+++ ibrs-buggy.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+# Creditos: https://www.cs.helsinki.fi/u/tpkarkka/opetus/14s/AfB/exercise2_problem3.py
+# Improved breakpoint reversal sort
+# Example:  ./prob3.py 8 2 7 6 5 1 4 3
+import sys
+
+if len(sys.argv) <= 2:
+    sys.exit("usage: " + sys.argv[0] + " [number 1] [number 2] ...")
+
+
+# Returns the number of breakpoins.
+# Equal to b() in the course book.
+# Requires linear time.
+def b(u):
+    assert u[0] == 0 and u[len(u)-1] == len(u) - 1
+    sum = 0
+    for i in range(len(u) - 1):
+        if u[i] != u[i+1] + 1 and u[i] != u[i+1] - 1:
+            # print "Breakpoint:", u[i], u[i+1] 
+            sum += 1
+    return sum
+
+# Reverses a range of given array in place.
+# Simulates the p(i,j) function from course book.
+# Requires linear time.
+def rev_range(u, i, j):
+    assert i <= j and i >= 0 and j >= 0
+    assert i < len(u) and j < len(u)
+    n = j - i + 1
+    for k in range(n/2): 
+        # Swap values of k and (n-k-1) in range [i,j]
+        u[i+k], u[i+(n-k-1)] = u[i+(n-k-1)], u[i+k]
+    return u
+
+# Returns true if given array has a decreasing strip.
+# Strips of length one are decreasing by definition (see course book).
+# Requires linear time.
+def has_decreasing(u):
+    assert u[0] == 0 and u[len(u)-1] == len(u) - 1
+    for i in range(1, len(u)-1):
+        # Element i belongs to a decreasing strip 
+        # if the element does not belong to an increasing strip.
+        if u[i-1] + 1 != u[i] and u[i] + 1 != u[i+1]:
+            # print "posible strip decreciente en: ", u[i-1], " ", u[i], " ", u[i + 1]
+            return True # u[i] is in decreasing stip
+    return False
+
+# Returns the segment whose reversal minimizes 
+# the number of breakpoints.
+# Brute-force search over all reversals.
+# Since b() and rev_range() require linear time,
+# this function requires in total O(n^2) time.
+def choose_reversal(u):
+    assert u[0] == 0 and u[len(u)-1] == len(u) - 1
+    max = 0
+    max_seg = (0,0)
+    br = b(u) # Original number of breakpoints
+    for i in range(1, len(u)-2):
+        for j in range(i+1, len(u)-1):
+            # Evaluate the reversal p(i,j):
+            # First check that at least one breakpoint is removed.
+            # This check is not necessary but improves time complexity
+            # because there are at most 2b reversals that pass the test.
+            # This constant time check is performed O(n^2) times.
+            if abs(u[i]-u[j+1]) == 1 or abs(u[i-1]-u[j]) == 1:
+                # Count how many breakpoints are removed by reversal p(i,j)
+                # Passing a shallow copy so that array u does not get modified.
+                # This takes O(n) time but is executed at most 2b times.
+                a = rev_range(u[:], i, j)            
+                if br - b(a) > max:
+                    max = br - b(a)
+                    max_seg = (i,j)
+                    if max == 2:   # Best we can find
+                        return (i,j)
+    return max_seg
+
+# Returns a segment containing an increasing strip.
+# Chooses always the leftmost increasing strip
+# (excluding the special elements u[0] and u[len(u)-1]).
+# Requires linear time.
+def choose_increasing(u):
+    assert u[0] == 0 and u[len(u)-1] == len(u) - 1
+    i = 1
+    while i < len(u)-1 and u[i] + 1 != u[i+1]:
+        i += 1
+    assert i < len(u)-1
+    j = i + 1
+    while j < len(u)-2 and u[j] + 1 == u[j+1]:
+        j += 1
+    return (i, j)
+
+# Implements the improved breakpoint reversal sort.
+# The while-loop iterates O(b) times where b is the number of
+# breakpoints. The function choose_reversal() requires
+# O(n^2) time, thus, the total time complexity
+# is O(b \times n^2) where n is the number of elements.
+def improvedBreakpointReversalSort(u):
+    while b(u) > 0:
+        if has_decreasing(u):
+            (i, j) = choose_reversal(u)
+            rev_range(u, i, j)
+        else:
+            (i, j) = choose_increasing(u)
+            rev_range(u, i, j)
+        print u, "b(u) =", b(u)
+    return u
+
+# Convert to integers (because sys.argv contains strings)
+ints = [int(i) for i in sys.argv[1:]]
+
+# print "ints =", ints
+
+# Sanity check:
+for i in ints:
+    assert i > 0 and i <= len(ints)
+
+# Add begin and end markers 0 and n+1
+u = [0] + ints + [len(ints) + 1]
+print u, "b(u) =", b(u)
+improvedBreakpointReversalSort(u)
--- ibrs.py
+++ ibrs.py
@@ -1,109 +1,84 @@
 #!/usr/bin/env python
-# Creditos: https://www.cs.helsinki.fi/u/tpkarkka/opetus/14s/AfB/exercise2_problem3.py
 # Improved breakpoint reversal sort
-# Example:  ./prob3.py 8 2 7 6 5 1 4 3
+# Example:  python ibrs.py 1 2 3 4 5 6 9 7 8
+# Credits: http://www.csbio.unc.edu/mcmillan/Media/breakpointReversalSort.txt
+# import random
 import sys
+
+def hasBreakpoints(seq):
+    """ returns True if sequnces in not strictly increasing by 1 """
+    for i in xrange(1, len(seq)):
+        if (seq[i] != seq[i-1] + 1):
+            return True
+    return False
+
+def getStrips(seq):
+    """ find contained intervals where sequence is ordered, and return intervals
+    in as lists, increasing and decreasing. Single elements are considered
+    decreasing. "Contained" excludes the first and last interval. """
+    deltas = [seq[i+1] - seq[i] for i in xrange(len(seq)-1)]
+    # print "deltas -> ", deltas
+    increasing = list()
+    decreasing = list()
+    start = 0
+
+    # print "enumerate(deltas) -> ", enumerate(deltas)
+    for i, diff in enumerate(deltas):
+        if (abs(diff) == 1) and (diff == deltas[start]):
+            continue
+        if (start > 0):
+            if deltas[start] == 1:
+                increasing.append((start, i+1))
+            else:
+                decreasing.append((start, i+1))
+        start = i+1
+    return increasing, decreasing
+
+def pickReversal(seq, strips):
+    """ test each decreasing interval to see if it leads to a reversal that
+    removes two breakpoints, otherwise, return a reversal that removes only one """
+    reversal = (-1, None)
+    left = [i for i, j in strips]
+    right = [j for i, j in strips]
+    for i in left:
+        for j in right:
+            if (i >= j-1):
+                # skip invalid intervals and
+                # those with only one element
+                continue
+            breakpointsRemoved = 0
+            if (abs(seq[j-1] - seq[i-1]) == 1):
+                # reversal will remove left breakpoint
+                breakpointsRemoved += 1
+            if (abs(seq[j] - seq[i]) == 1):
+                # reversal will remove right breakpoint
+                breakpointsRemoved += 1
+            if (breakpointsRemoved > reversal[0]):
+                reversal = (breakpointsRemoved, (i,j))
+    print "%d:" % reversal[0],
+    return reversal[1]
+
+def doReversal(seq,(i,j)):
+    return seq[:i] + [element for element in reversed(seq[i:j])] + seq[j:]
+
+def improvedBreakpointReversalSort(seq):
+    while hasBreakpoints(seq):
+        increasing, decreasing = getStrips(seq)
+        # print "increasing ", increasing
+        # print "decreasing ", decreasing
+
+        if len(decreasing) > 0:
+            reversal = pickReversal(seq, increasing+decreasing)
+        else:
+            print "0:",
+            reversal = increasing[0]
+        print seq, "reversal", reversal
+        seq = doReversal(seq,reversal)
+    print "s:", seq, "Sorted"
+    return
 
 if len(sys.argv) <= 2:
     sys.exit("usage: " + sys.argv[0] + " [number 1] [number 2] ...")
-
-
-# Returns the number of breakpoins.
-# Equal to b() in the course book.
-# Requires linear time.
-def b(u):
-    assert u[0] == 0 and u[len(u)-1] == len(u) - 1
-    sum = 0
-    for i in range(len(u) - 1):
-        if u[i] != u[i+1] + 1 and u[i] != u[i+1] - 1:
-            # print "Breakpoint:", u[i], u[i+1] 
-            sum += 1
-    return sum
-
-# Reverses a range of given array in place.
-# Simulates the p(i,j) function from course book.
-# Requires linear time.
-def rev_range(u, i, j):
-    assert i <= j and i >= 0 and j >= 0
-    assert i < len(u) and j < len(u)
-    n = j - i + 1
-    for k in range(n/2): 
-        # Swap values of k and (n-k-1) in range [i,j]
-        u[i+k], u[i+(n-k-1)] = u[i+(n-k-1)], u[i+k]
-    return u
-
-# Returns true if given array has a decreasing strip.
-# Strips of length one are decreasing by definition (see course book).
-# Requires linear time.
-def has_decreasing(u):
-    assert u[0] == 0 and u[len(u)-1] == len(u) - 1
-    for i in range(1, len(u)-1):
-        # Element i belongs to a decreasing strip 
-        # if the element does not belong to an increasing strip.
-        if u[i-1] + 1 != u[i] and u[i] + 1 != u[i+1]:
-            # print "posible strip decreciente en: ", u[i-1], " ", u[i], " ", u[i + 1]
-            return True # u[i] is in decreasing stip
-    return False
-
-# Returns the segment whose reversal minimizes 
-# the number of breakpoints.
-# Brute-force search over all reversals.
-# Since b() and rev_range() require linear time,
-# this function requires in total O(n^2) time.
-def choose_reversal(u):
-    assert u[0] == 0 and u[len(u)-1] == len(u) - 1
-    max = 0
-    max_seg = (0,0)
-    br = b(u) # Original number of breakpoints
-    for i in range(1, len(u)-2):
-        for j in range(i+1, len(u)-1):
-            # Evaluate the reversal p(i,j):
-            # First check that at least one breakpoint is removed.
-            # This check is not necessary but improves time complexity
-            # because there are at most 2b reversals that pass the test.
-            # This constant time check is performed O(n^2) times.
-            if abs(u[i]-u[j+1]) == 1 or abs(u[i-1]-u[j]) == 1:
-                # Count how many breakpoints are removed by reversal p(i,j)
-                # Passing a shallow copy so that array u does not get modified.
-                # This takes O(n) time but is executed at most 2b times.
-                a = rev_range(u[:], i, j)            
-                if br - b(a) > max:
-                    max = br - b(a)
-                    max_seg = (i,j)
-                    if max == 2:   # Best we can find
-                        return (i,j)
-    return max_seg
-
-# Returns a segment containing an increasing strip.
-# Chooses always the leftmost increasing strip
-# (excluding the special elements u[0] and u[len(u)-1]).
-# Requires linear time.
-def choose_increasing(u):
-    assert u[0] == 0 and u[len(u)-1] == len(u) - 1
-    i = 1
-    while i < len(u)-1 and u[i] + 1 != u[i+1]:
-        i += 1
-    assert i < len(u)-1
-    j = i + 1
-    while j < len(u)-2 and u[j] + 1 == u[j+1]:
-        j += 1
-    return (i, j)
-
-# Implements the improved breakpoint reversal sort.
-# The while-loop iterates O(b) times where b is the number of
-# breakpoints. The function choose_reversal() requires
-# O(n^2) time, thus, the total time complexity
-# is O(b \times n^2) where n is the number of elements.
-def improvedBreakpointReversalSort(u):
-    while b(u) > 0:
-        if has_decreasing(u):
-            (i, j) = choose_reversal(u)
-            rev_range(u, i, j)
-        else:
-            (i, j) = choose_increasing(u)
-            rev_range(u, i, j)
-        print u, "b(u) =", b(u)
-    return u
 
 # Convert to integers (because sys.argv contains strings)
 ints = [int(i) for i in sys.argv[1:]]
@@ -116,5 +91,5 @@ for i in ints:
 
 # Add begin and end markers 0 and n+1
 u = [0] + ints + [len(ints) + 1]
-print u, "b(u) =", b(u)
+print "o:", u
 improvedBreakpointReversalSort(u)
